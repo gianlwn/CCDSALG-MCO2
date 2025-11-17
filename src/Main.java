f1639aea00be1dcc90112970695cfb673d5a70fd
cmysrbia <erikaghirl@gmail.com>
2025-11-17 19:23:24 +0800

    rev: updated

--- src/Main.java
+++ src/Main.java
@@ -3,42 +3,44 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-        Scanner scn = new Scanner(System.in);
-        System.out.print("Input file path: ");
-        String filePath = scn.nextLine();
+        try (Scanner scn = new Scanner(System.in)) {
+            
+            System.out.print("Input file path: ");
+            String filePath = scn.nextLine();
 
-        Graph graph = new Graph();
-        graph.loadGraph(filePath);
+            Graph graph = new Graph();
+            graph.loadGraph(filePath);
 
-        while(true){
-            System.out.println("\nMAIN MENU");
-            System.out.println("[1] Get friend list");
-            System.out.println("[2] Get connection");
-            System.out.println("[3] Exit");
-            System.out.println("Enter your choice: ");
-            int choice = scn.nextInt();
+            while(true){
+                System.out.println("\nMAIN MENU");
+                System.out.println("[1] Get friend list");
+                System.out.println("[2] Get connection");
+                System.out.println("[3] Exit");
+                System.out.print("\nEnter your choice: ");
+                int choice = scn.nextInt();
 
-            switch(choice){
-                case 1 -> {
-                    System.out.println("Enter ID of person: ");
-                    int id = scn.nextInt();
-                    // get friend list
+                switch(choice){
+                    case 1 -> {
+                        System.out.print("Enter ID of person: ");
+                        int id = scn.nextInt();
+                        graph.displayFriendList(id);
+                    }
+
+                    case 2 -> {
+                        System.out.print("Enter ID of first person: ");
+                        int id1 = scn.nextInt();
+                        System.out.print("Enter ID of second person: ");
+                        int id2 = scn.nextInt();
+                        graph.displayConnections(id1, id2);
+                    }
+
+                    case 3 -> {
+                        System.out.println("Exiting program.");
+                        System.exit(0);
+                    }
+                    default -> System.out.println("Invalid choice! Please enter 1, 2, or 3.");
+                        
                 }
-
-                case 2 -> {
-                    System.out.println("Enter ID of first person: ");
-                    int id1 = scn.nextInt();
-                    System.out.println("Enter ID of second person: ");
-                    int id2 = scn.nextInt();
-                    // get connection
-                }
-
-                case 3 -> {
-                    System.out.println("Exiting program.");
-                    System.exit(0);
-                }
-                default -> System.out.println("Invalid choice! Please enter 1, 2, or 3.");
-                    
             }
         }
     }
